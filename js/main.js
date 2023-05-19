0a62ee85cee0df8f4f96ecbfcd53f49a353555ec
bruxo <diogo.stein@outlook.com.br>
2023-05-19 12:22:19 -0300

    Add some dummy content

---
+++ js/main.js
@@ -0,0 +1,59 @@
+// const wrapper = document.querySelector('.wrapper');
+// const card = document.querySelector('.card');
+// const bookTitle = document.querySelector('#book-title');
+// const bookAuthor = document.querySelector('#book-author');
+// const bookPages = document.querySelector('#book-pages');
+// const bookRead = document.querySelector('#book-read');
+
+// let card1 = document.createElement('div');
+
+// let myLibrary = [
+//   {
+//     title: 'A',
+//     author: 'A',
+//     pages: 100,
+//     read: false,
+//   },
+//   {
+//     title: 'B',
+//     author: 'B',
+//     pages: 100,
+//     read: false,
+//   },
+//   {
+//     title: 'C',
+//     author: 'C',
+//     pages: 100,
+//     read: false,
+//   },
+//   {
+//     title: 'D',
+//     author: 'D',
+//     pages: 100,
+//     read: false,
+//   },
+// ];
+
+// function Book(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+
+//   this.info = function () {
+//     return `${title} by ${author}, ${pages} pages, ${read}`;
+//   };
+// }
+
+// function addBookToLibrary() {
+//   // do stuff here
+// }
+
+// function displayBooks() {
+//   for (book of myLibrary) {
+//     console.table(book);
+//     wrapper.innerHTML += card;
+//   }
+// }
+
+// displayBooks();
